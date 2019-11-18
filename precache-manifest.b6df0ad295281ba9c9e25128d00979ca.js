7a929ba4da35a7265763eccad2e354a045c080db
Shirley Xiaolin Xu <shirleyxiaolinxu@gmail.com>
2019-11-18 15:23:33 -0800

    Updates

---
+++ precache-manifest.b6df0ad295281ba9c9e25128d00979ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bc4178f53a1ddb18fe60e2bbbaf1e17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80a4f65873402578d81e",
+    "url": "/static/css/main.0968aaa8.chunk.css"
+  },
+  {
+    "revision": "3d23430ff6fef1a42755",
+    "url": "/static/js/2.feadcfb6.chunk.js"
+  },
+  {
+    "revision": "80a4f65873402578d81e",
+    "url": "/static/js/main.80538231.chunk.js"
+  },
+  {
+    "revision": "a6e3dc98bf14d00cdb8a",
+    "url": "/static/js/runtime-main.c5683173.js"
+  }
+]);
