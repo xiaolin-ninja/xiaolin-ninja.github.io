a7ade4e49465176d8a1cf0fb4f5f9078eca695ab
Shirley Xiaolin Xu <xiaolin.xu@salesforce.com>
2020-06-12 03:46:48 -0700

    remove stupid wording

--- src/skills.jsx
+++ src/skills.jsx
@@ -14,12 +14,12 @@ class Skills extends Component {
           <Grid>
             <Row>
               <Description title='Well-Rounded'
-              body="I code daily in Ruby, Go, & SQL at Salesforce (Heroku), and in C, C++ & Python at NYU. I created this portfolio with ReactJS, and analyzed data with STATA & R at UCSD. Away from the internet, I train martial arts, volunteer in marine conservation (Rescue Diver), and ran a talent agency with a guy nicknamed 'Sexy Stalin'. I seamlessly transition between support & leadership, and easily adapt to new environments."/>
+              body="I build RESTful & GraphQL APIs and optimize the scalability of a Cloud PaaS by day, and explore security vulnerabilities in complex systems by night. My multicultural background & interdisciplinary training enable me to adapt quickly to new environments. I hold a Masters in International Affairs from UCSD, specialized in Business Management & Corporate Social Responsibility; I switched careers to software engineering to chase dreams and challenge convention."/>
 
               <Description title='Not Stupid' body='I went from, "What is git?" to productionizing a web application with user auth & a dynamic database in 10 weeks. I presented "BudBud: A Buddy for your Bud" at Hackbright Academy graduation.
-              Straight out of bootcamp, I dove into an enterprise codebase with an on-call rotation, with no prior exposure to the languages, libraries, and tooling. I recently built a TLS/SSL VPN from scratch on VMs... because grad school is wild.'/>
+              I dove into an enterprise codebase and an on-call rotation, with no prior exposure to the languages, libraries, or tooling, & shipped an incident-preventing feature my second month. I recently built a TLS/SSL VPN from scratch on VMs... because grad school is wild.'/>
 
-              <Description title='Over-Achiever' body="I am pursuing a MS in Cybersecurity while working full-time. I build RESTful & GraphQL APIs, and optimize scalability of enterprise applications. I am interested in Access Management & Penetration Testing, and aspire to lead a Red Team some day. I hold a Masters in International Affairs from UCSD, specialized in Corporate Social Responsibility. I switched careers to software engineering to chase dreams and challenge convention." />
+              <Description title='"#Rockstar"' body="I am pursuing a MS in Cybersecurity at NYU while working full time. I aim to specialize in IoT Security, & aspire to lead a Red Team some day. I code daily in Ruby, Go, & SQL at Salesforce (Heroku), and in C & Python at NYU. I created this portfolio with ReactJS, and analyzed data with STATA & R at UCSD. Away from the internet, I train martial arts, volunteer in marine conservation (Rescue Diver), and ran a talent agency with a dude nicknamed 'Sexy Stalin'. " />
             </Row>
           </Grid>
           <Row>
