ac545abda8d93ff681cb44d0e64e65ca86b45f5a
Shirley Xiaolin Xu <xiaolin.xu@salesforce.com>
2020-08-13 13:12:27 -0700

    vice

--- src/company.jsx
+++ src/company.jsx
@@ -28,7 +28,7 @@ class Company extends Component {
 class Header extends Component {
   render() {
     return (
-      <div className='sectionHeader'>Why Slack?</div>
+      <div className='sectionHeader'>Why VICE?</div>
     )
   }
 }
@@ -37,16 +37,16 @@ class Description extends Component {
   render() {
     const a = {
       'paddingBottom': '20px',
-      'color': '#000'
+      'color': '#fff'
     }
 
     return (
       <div style={a}>
-        Now more than ever, Slack is an indispensible tool for productivity across the world. I've been impressed by Slack leadership's transparency in blog posts, and the company's ability to continually innovate and rapidly scale while remaining reliable and reactive to customer needs -- like the new Enterprise Key Management feature. A good friend of mine on the Internal Tools team has sang praises of the great culture at Slack, and it sounds like the mid-sized, mission-driven company of my dreams.
+        Bold, honest, and unapologetically authentic. Those are words I would use to describe myself, and adjectives that have made me a loyal consumer of VICE for years. I am a highly mission-driven person, and if I am going to spend 50% of my waking hours contributing to a corporation, I want to spend it protecting the data and integrity of an impactful modern media company with ethical, hard-hitting content that inspires and invigorates generations.
         <p/>
         I've dedicated 2.5 years at Heroku to optimizing the scalability of one of the oldest and largest cloud Platforms as a Service. One of my first projects was creating internal tooling to automate fraud detection. I currently help build Salesforce Evergreen, a serverless Function as a Service, from the ground up --- while pursing a Masters degree in Cybersecurity. In past careers, I helped businesses expand to global markets & wrote a published graduate school thesis on the political & economic impacts of the Deep Water Horizon oil spill.
         <p/>
-        I bring to the table a unique combination of experiences in production engineering, security, entrepreneurship, and international policy, and a passion for action. I can't wait to help keep Slack customer data safe.
+        I bring to the table a unique combination of experiences in production engineering, security, entrepreneurship, and international policy, and a passion for action. I can't wait to help keep VICE Media secure.
       </div>
     )
   }
