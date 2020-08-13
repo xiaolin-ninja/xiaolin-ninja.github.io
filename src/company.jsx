d552cc03b5c61564015d4eedb128333c06799e4b
Shirley Xiaolin Xu <xiaolin.xu@salesforce.com>
2020-08-13 13:20:03 -0700

    fix math

--- src/company.jsx
+++ src/company.jsx
@@ -42,7 +42,7 @@ class Description extends Component {
 
     return (
       <div style={a}>
-        Bold, honest, and unapologetically authentic. Those are words I would use to describe myself, and adjectives that have made me a loyal consumer of VICE for years. I am a highly mission-driven person, and if I am going to spend 50% of my waking hours contributing to a corporation, I want to spend it protecting the data and integrity of an impactful modern media company with ethical, hard-hitting content that inspires and invigorates generations.
+        Bold, honest, and unapologetically authentic. Those are words I would use to describe myself, and adjectives that have made me a loyal consumer of VICE for years. I am a highly mission-driven person, and if I am going to spend 60% of my waking hours contributing to a corporation, I want to spend it protecting the data and integrity of an impactful modern media company with ethical, hard-hitting content that inspires and invigorates generations.
         <p/>
         I've dedicated 2.5 years at Heroku to optimizing the scalability of one of the oldest and largest cloud Platforms as a Service. One of my first projects was creating internal tooling to automate fraud detection. I currently help build Salesforce Evergreen, a serverless Function as a Service, from the ground up --- while pursing a Masters degree in Cybersecurity. In past careers, I helped businesses expand to global markets & wrote a published graduate school thesis on the political & economic impacts of the Deep Water Horizon oil spill.
         <p/>
