a0a8f6de53ba5957f73d8221972acfac0cd5ba9e
Shirley Xiaolin Xu <xiaolin.xu@salesforce.com>
2020-08-13 23:17:34 -0700

    typo

--- src/company.jsx
+++ src/company.jsx
@@ -46,7 +46,7 @@ class Description extends Component {
         <p/>
         I've dedicated 2.5 years at Heroku to optimizing the scalability of one of the oldest and largest cloud Platforms as a Service. One of my first projects was creating internal tooling to automate fraud detection. I currently help build Salesforce Evergreen, a serverless Function as a Service, from the ground up --- while pursing a Masters degree in Cybersecurity. In past careers, I helped businesses expand to global markets & wrote a published graduate school thesis on the political & economic impacts of the Deep Water Horizon oil spill.
         <p/>
-        I bring to the table a unique combination of experiences in production engineering, security, entrepreneurship, and international policy, and a passion for action. I can't wait to help keep Rockstar players, employees, and intellectual property safe.
+        I bring to the table a unique combination of experiences in production engineering, security, entrepreneurship, and international policy, as well as a passion for action. I can't wait to help keep Rockstar players, employees, and intellectual property safe.
       </div>
     )
   }
